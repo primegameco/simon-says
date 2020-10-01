3767dc1174eb090d689e41420c066c11f69f8f3c
Mithil Verma <mithilproof@Mithils-MacBook-Pro.local>
2020-10-01 13:32:26 +0530

    Updates

---
+++ precache-manifest.44561025030182551ac1c51a2f502211.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60b7feb8eba258b2b59b80701014541",
+    "url": "/simon-says/index.html"
+  },
+  {
+    "revision": "a194e1200acb2a0a274d",
+    "url": "/simon-says/static/css/main.d93ef70b.chunk.css"
+  },
+  {
+    "revision": "0ca8a90f357df031107f",
+    "url": "/simon-says/static/js/2.47111d83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simon-says/static/js/2.47111d83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a194e1200acb2a0a274d",
+    "url": "/simon-says/static/js/main.be8b9b71.chunk.js"
+  },
+  {
+    "revision": "e07847e5e3a960d9dfed",
+    "url": "/simon-says/static/js/runtime-main.507f556a.js"
+  },
+  {
+    "revision": "d6f8a4c1c4fce8d9ad93c0bd33b478b7",
+    "url": "/simon-says/static/media/blue.d6f8a4c1.mp3"
+  },
+  {
+    "revision": "3c8ca9470540bbbea8eaa187bfe82a39",
+    "url": "/simon-says/static/media/green.3c8ca947.mp3"
+  },
+  {
+    "revision": "8d481077276f07445e615252af9a8be4",
+    "url": "/simon-says/static/media/red.8d481077.mp3"
+  },
+  {
+    "revision": "3ee582459fb09df5b041ef1b9593d7b5",
+    "url": "/simon-says/static/media/yellow.3ee58245.mp3"
+  }
+]);
